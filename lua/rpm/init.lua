54655c0f6f3121ec17dccb122ae0ccc01ad30e95
Niza Toshpulatov <niza@hey.com>
2024-03-20 13:57:56 +0100

    Wait until plugins initialization

--- lua/rpm/init.lua
+++ lua/rpm/init.lua
@@ -4,19 +4,29 @@ local plugin_list = require("rpm.plugin_list")
 local function init_plugins()
   print("Initializing plugins...")
 
+  local routines = {}
+
   for _, plugin in pairs(plugin_list) do
     local routine = coroutine.create(function()
       if core.is_plugin_installed(plugin.path) and plugin.init_fn then
         plugin.init_fn()
       end
-
-      if plugin == plugin_list[#plugin_list] then
-        print("Plugins initialized.")
-      end
     end)
 
     coroutine.resume(routine)
+    table.insert(routines, routine)
   end
+
+  vim.wait(100, function()
+    for _, routine in pairs(routines) do
+      if coroutine.status(routine) ~= "dead" then
+        return false
+      end
+    end
+
+    print("Plugins initialized.")
+    return true
+  end)
 end
 
 local init_routine = coroutine.create(init_plugins)
