d525904580d3777edb811f054d11e04c62dc1730
Niza Toshpulatov <niza@hey.com>
2024-05-14 01:43:14 +0200

    Refactor modules

--- lua/rpm/core.lua
+++ lua/rpm/core.lua
@@ -1,4 +1,6 @@
-local function parse_input_answer(answer)
+local M = {}
+
+function M.parse_input_answer(answer)
   if answer:lower():match("^y") then
     return true
   end
@@ -6,12 +8,12 @@ local function parse_input_answer(answer)
   return false
 end
 
-local function get_plugin_version(install_path)
+function M.get_plugin_version(install_path)
   return vim.fn.isdirectory(install_path) == 0 and "Not installed" or
-    vim.fn.system({ "git", "-C", install_path, "describe", "--tags" }):gsub("\n", "")
+      vim.fn.system({ "git", "-C", install_path, "describe", "--tags" }):gsub("\n", "")
 end
 
-local function coerce_to_table(value)
+function M.coerce_to_table(value)
   if type(value) == "table" then
     return value
   end
@@ -19,7 +21,7 @@ local function coerce_to_table(value)
   return { value }
 end
 
-local silent_print = function(message, silent)
+function M.silent_print(message, silent)
   silent = silent or false
 
   if not silent then
@@ -27,8 +29,8 @@ local silent_print = function(message, silent)
   end
 end
 
-local function get_plugin_info(path)
-  local paths = coerce_to_table(path)
+function M.get_plugin_info(path)
+  local paths = M.coerce_to_table(path)
 
   -- Consider the last path as the main dependency
   local last_path = paths[#paths]
@@ -36,7 +38,7 @@ local function get_plugin_info(path)
   local name = last_path:match("([^/]+)$"):gsub("%.[a-zA-Z0-9]+", "")
   local install_path = vim.fn.stdpath("config") .. "/pack/plugins/start/" .. name
 
-  local version = get_plugin_version(install_path)
+  local version = M.get_plugin_version(install_path)
 
   return {
     install_path = install_path,
@@ -46,14 +48,14 @@ local function get_plugin_info(path)
   }
 end
 
-local function is_plugin_installed(path, silent)
-  local paths = coerce_to_table(path) 
+function M.is_plugin_installed(path, silent)
+  local paths = M.coerce_to_table(path)
 
   for _, p in ipairs(paths) do
-    local info = get_plugin_info(p)
+    local info = M.get_plugin_info(p)
 
     if vim.fn.isdirectory(info.install_path) == 0 then
-      silent_print("Dependency " .. info.name .. " is not installed.", silent)
+      M.silent_print("Dependency " .. info.name .. " is not installed.", silent)
       return false
     end
   end
@@ -61,48 +63,48 @@ local function is_plugin_installed(path, silent)
   return true
 end
 
-local function generate_helptags(path, silent)
-  local info = get_plugin_info(path)
+function M.generate_helptags(path, silent)
+  local info = M.get_plugin_info(path)
   local doc_dir = info.install_path .. "/doc"
 
   if vim.fn.isdirectory(doc_dir) == 1 then
-    silent_print("Generating help tags for " .. info.name .. "...", silent)
+    M.silent_print("Generating help tags for " .. info.name .. "...", silent)
 
     vim.cmd("helptags " .. doc_dir)
-    
-    silent_print("Help tags for " .. info.name .. " have been generated!", silent)
+
+    M.silent_print("Help tags for " .. info.name .. " have been generated!", silent)
   end
 end
 
-local function install_plugin(path, silent)
-  local paths = coerce_to_table(path)
+function M.install_plugin(path, silent)
+  local paths = M.coerce_to_table(path)
 
   for _, p in ipairs(paths) do
-    if is_plugin_installed(p, true) then
-      silent_print("Dependency " .. p .. " is already installed.\n", silent)
+    if M.is_plugin_installed(p, true) then
+      M.silent_print("Dependency " .. p .. " is already installed.\n", silent)
       goto continue
     end
 
     local url = p:match("^http") and p or "https://github.com/" .. p
-    local info = get_plugin_info(p)
+    local info = M.get_plugin_info(p)
 
-    silent_print("Cloning " .. url .. " to " .. info.install_path .. "...", silent)
+    M.silent_print("Cloning " .. url .. " to " .. info.install_path .. "...", silent)
 
     vim.fn.system({ "git", "clone", url, info.install_path })
 
-    generate_helptags(p, silent)
+    M.generate_helptags(p, silent)
 
-    silent_print("Dependency " .. info.name .. " has been installed!", silent)
+    M.silent_print("Dependency " .. info.name .. " has been installed!", silent)
     ::continue::
   end
-end 
+end
 
-local function update_plugin(path, silent)
-  local paths = coerce_to_table(path)
+function M.update_plugin(path, silent)
+  local paths = M.coerce_to_table(path)
 
   for _, p in ipairs(paths) do
-    if not is_plugin_installed(p) then
-      silent_print("Dependency " .. p .. " is not installed.", silent)
+    if not M.is_plugin_installed(p) then
+      M.silent_print("Dependency " .. p .. " is not installed.", silent)
 
       local answer = "n"
 
@@ -110,7 +112,7 @@ local function update_plugin(path, silent)
         answer = vim.fn.input("Would you like to install it? (y/n): ")
       end
 
-      if not parse_input_answer(answer) then
+      if not M.parse_input_answer(answer) then
         goto continue
       end
 
@@ -118,64 +120,56 @@ local function update_plugin(path, silent)
         print("\n")
       end
 
-      install_plugin(p, silent)
+      M.install_plugin(p, silent)
       goto continue
     end
 
-    local info = get_plugin_info(p)
+    local info = M.get_plugin_info(p)
 
-    silent_print("Updating dependency " .. info.name .. "...", silent)
+    M.silent_print("Updating dependency " .. info.name .. "...", silent)
 
     vim.fn.system({ "git", "-C", info.install_path, "pull" })
 
-    local new_version = get_plugin_version(info.install_path)
+    local new_version = M.get_plugin_version(info.install_path)
 
     if info.version == new_version then
-      silent_print("Dependency " .. info.name .. " is already up to date.", silent)
+      M.silent_print("Dependency " .. info.name .. " is already up to date.", silent)
     else
-      silent_print("Dependency " .. info.name .. " has been updated to " .. new_version .. "!", silent)
+      M.silent_print("Dependency " .. info.name .. " has been updated to " .. new_version .. "!", silent)
     end
 
     ::continue::
   end
 end
 
-local function delete_plugin(path, silent)
-  local paths = coerce_to_table(path)
+function M.delete_plugin(path, silent)
+  local paths = M.coerce_to_table(path)
 
   for _, p in ipairs(paths) do
-    local info = get_plugin_info(p)
+    local info = M.get_plugin_info(p)
 
     if vim.fn.isdirectory(info.install_path) == 0 then
-      silent_print("Dependency " .. info.name .. " is not installed.\n", silent)
+      M.silent_print("Dependency " .. info.name .. " is not installed.\n", silent)
       goto continue
     end
 
     if not silent then
       local answer = vim.fn.input("Are you sure you want to delete " .. info.name .. "? (y/n): ")
 
-      if not parse_input_answer(answer) then
+      if not M.parse_input_answer(answer) then
         goto continue
       end
 
       print("\n")
     end
 
-    silent_print("Deleting " .. info.name .. "...", silent)
+    M.silent_print("Deleting " .. info.name .. "...", silent)
 
     vim.fn.system({ "rm", "-rf", info.install_path })
 
-    silent_print("Dependency " .. info.name .. " has been deleted.", silent)
+    M.silent_print("Dependency " .. info.name .. " has been deleted.", silent)
     ::continue::
   end
 end
 
-return {
-  delete_plugin = delete_plugin,
-  get_plugin_info = get_plugin_info,
-  install_plugin = install_plugin, 
-  is_plugin_installed = is_plugin_installed,
-  parse_input_answer = parse_input_answer,
-  generate_helptags = generate_helptags,
-  update_plugin = update_plugin
-}
+return M
